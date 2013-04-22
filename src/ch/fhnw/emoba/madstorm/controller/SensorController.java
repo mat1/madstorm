c2396c20a7a07c157a113e0298e65e2cf968c4ff
Florian Luescher <florian@florianluescher.ch>
2013-04-22 20:58:57 +0200

    Fix ident

--- src/ch/fhnw/emoba/madstorm/controller/SensorController.java
+++ src/ch/fhnw/emoba/madstorm/controller/SensorController.java
@@ -31,12 +31,12 @@ public class SensorController implements Controller, SensorEventListener {
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		switch (event.sensor.getType()) {
-        case Sensor.TYPE_ACCELEROMETER:
-            System.arraycopy(event.values, 0, valuesAccel, 0, 3);
-            break;
-        case Sensor.TYPE_MAGNETIC_FIELD:
-            System.arraycopy(event.values, 0, valuesMagnet, 0, 3);
-            break;
+		case Sensor.TYPE_ACCELEROMETER:
+			System.arraycopy(event.values, 0, valuesAccel, 0, 3);
+			break;
+		case Sensor.TYPE_MAGNETIC_FIELD:
+			System.arraycopy(event.values, 0, valuesMagnet, 0, 3);
+			break;
 		}
 	}
 	
